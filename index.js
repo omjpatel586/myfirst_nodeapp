8df177db5ba5d87658fb170dc1a93ac93872c90d
ompatel616 <omjpatel6461@gmail.com>
2023-03-08 11:38:59 +0530

    success

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ http.createServer((resq, resp) => {
     resp.writeHead(200 ,{'Content-Type': 'html'})
     resp.write("<div style='background-color: yellow'>")
     resp.write("<h1 style='color: red'>Hello World</h1>")
+    resp.write("<h1 style='color: green'>Hello Good Morning</h1>")
     resp.write("<h1>Hello</h1>")
     resp.write("</div>")
     resp.end()
